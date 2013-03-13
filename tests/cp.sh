48cb183ddb0f94440cfc6a6c373bf65ae364bbbc
Radek Pazdera <rpazdera@redhat.com>
2013-03-13 23:45:10 +0100

    tests: Adding some more information to the cp test

--- tests/cp.sh
+++ tests/cp.sh
@@ -4,5 +4,21 @@
 . tests/tests-common.sh
 
 now=`date +%s`
-run_test "cp -a $TEST_DIR $DROP_OFF_DIR/${now}-copy"
+target="$DROP_OFF_DIR/${now}-copy"
+run_test "cp -a $TEST_DIR $target"
+echo "[DONE]"
+
+let source_size=`ls -1a $TEST_DIR | wc -l`-2
+echo "  Source directory has $source_size files"
+
+let target_size=`ls -1a $target | wc -l`-2
+echo "  Target directory has $target_size files"
+
+let diff=$source_size-$target_size
+if [ $diff -ne 0 ]; then
+    echo -n "Copy operation did not finish ... [WARNING]"
+else
+    echo -n "Copying ... "
+fi
+
 rm -rf "$DROP_OFF_DIR/${now}-copy"
