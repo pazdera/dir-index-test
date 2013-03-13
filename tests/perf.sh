ba997639a6b3941c0a55bde62177be8a057b36da
Radek Pazdera <rpazdera@redhat.com>
2013-03-13 23:48:39 +0100

    tests: Adding perf and seekwatcher test classes

---
+++ tests/perf.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# This file contains some performance tests for file
+# system's directory index
+
+FS="$1"
+DEVICE="$2"
+TEST_DIR="$3"
+DROP_OFF_DIR="$4"
+RES_DIR="$5"
+
+PERF_DIR="$RES_DIR/perf"
+mkdir -p "$PERF_DIR"
+
+function time_benchmark
+{
+    local test_name=$1
+    local cmd=$2
+
+    sync && echo 3 > /proc/sys/vm/drop_caches
+    sleep 5
+
+    echo "Executing $test_name time benchmark"
+
+    result=`(time ($cmd 2>/dev/null >/dev/null)) 2>&1`
+    echo "$result" > "$PERF_DIR/$test_name.time"
+}
+
+# ls -l
+time_benchmark "lsl" "ls -l $TEST_DIR"
+
+# ls
+time_benchmark "ls" "ls $TEST_DIR"
+
+# getdents+stat
+time_benchmark "getdents-stat" "bin/getdents-stat $TEST_DIR"
+
+# getdents+stat
+time_benchmark "readdir-stat" "bin/readdir-stat $TEST_DIR"
+
+# find -name
+time_benchmark "find" "find $TEST_DIR -type f"
+
+# tar -cf
+time_benchmark "tar" "tar -cf - $TEST_DIR"
+
+# cp -a
+now=`date +%s`
+time_benchmark "cp" "cp -a $TEST_DIR $DROP_OFF_DIR/${now}-copy"
+rm -rf "$DROP_OFF_DIR/${now}-copy"
---
+++ tests/seekwatcher.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# This script contains several seekwatcher test cases that
+# can be used for assessing file system's directory indexes
+
+FS="$1"
+DEVICE="$2"
+TEST_DIR="$3"
+DROP_OFF_DIR="$4"
+RES_DIR="$5"
+
+SW_DIR="$RES_DIR/seekwatcher"
+mkdir -p "$SW_DIR"
+
+function seekwatcher_benchmark
+{
+    local test_name=$1
+    local cmd=$2
+
+    echo "Executing $test_name seekwatcher benchmark"
+
+    sync
+    echo 3 > /proc/sys/vm/drop_caches
+    sleep 5
+
+    blktrace -d "$DEVICE" -o "$SW_DIR/$test_name" >/dev/null &
+#            -a queue -a complete -a issue -b 2048 &
+    local blktrace_pid=`echo $!`
+
+    $cmd >/dev/null 2>/dev/null
+
+    kill -s SIGTERM $blktrace_pid
+    seekwatcher -t "$SW_DIR/$test_name" -o "$RES_DIR/$test_name.png" >/dev/null
+
+    # This doesn't work on my system.
+    # Sometimes it just hangs when the command is finished.
+    #(seekwatcher -t "$rdir/$test_name.trace" \
+    #        -o "$rdir/$test_name-seekwatcher.png" \
+    #        -p "$cmd 2>/dev/null >/dev/null" \
+    #        -d "$DEVICE") > "$rdir/$test_name.seekwatcher"
+}
+
+# ls -l
+seekwatcher_benchmark "lsl" "ls -l $TEST_DIR"
+
+# ls
+seekwatcher_benchmark "ls" "ls $TEST_DIR"
+
+# getdents+stat
+seekwatcher_benchmark "getdents-stat" "bin/getdents-stat $TEST_DIR"
+
+# readdir+stat
+seekwatcher_benchmark "readdir-stat" "bin/readdir-stat $TEST_DIR"
+
+# find -name
+seekwatcher_benchmark "find" "find $TEST_DIR -type f"
+
+# tar -cf
+seekwatcher_benchmark "tar" "tar -cf - $TEST_DIR"
+
+# cp -a
+now=`date +%s`
+seekwatcher_benchmark "cp" "cp -a $TEST_DIR $DROP_OFF_DIR/${now}-copy"
+rm -rf "$DROP_OFF_DIR/${now}-copy"
